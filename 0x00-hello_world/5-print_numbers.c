4dcc6d6fd2194b5ccc425b15bcfa83b2785c5605
Licia27 <vilakaziandile704@gamil.com>
2023-06-18 06:15:32 -0700

    numbers

--- 0x00-hello_world/5-print_numbers.c
+++ 0x00-hello_world/5-print_numbers.c
@@ -1,28 +1,20 @@
-/*
- * File: 5-print_numbers.c
- * Auth: Brennan D Baraban
- */
-
-
 #include <stdio.h>
 
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0.
+ * main - Prints numbers from 0 to 9.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
+	int i;
 
 
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-
-
-	printf("\n");
-
-
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
 	return (0);
 }
