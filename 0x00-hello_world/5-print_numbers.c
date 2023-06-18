34c654b2078cb9cc0b74d5948b9836f13f0e6824
Licia27 <vilakaziandile704@gamil.com>
2023-06-18 05:48:14 -0700

    numbers

--- 0x00-hello_world/5-print_numbers.c
+++ 0x00-hello_world/5-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Prints numbers from 0 to 9.
  *
@@ -9,10 +10,11 @@ int main(void)
 {
 	int i;
 
+
 	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
